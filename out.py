d7198671a3f19d925188fa2aa9bf75bb817a0e11
Steam <steampunkv@gmail.com>
2017-12-28 18:16:00 +0100

    added minimum word length

--- out.py
+++ out.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 import random
 from saveAndLoad import SaveAndLoad
+
 previous_char = ' '
 word = ''
 file_in = input("path to data file: ")
 my_obj = SaveAndLoad(file_in)
 
 times = int(input("how many words?: "))
+min_word_length = int(input("what is the minimum word legnth?"))
 while times > 0:
     while True:
         previous_char = random.choices(my_obj.give_keys(previous_char), weights=my_obj.give_values(previous_char))[0]
         word += previous_char
         if previous_char == ' ':
             break
-    print(word)
+    if min_word_length <= len(word):
+        print(word)
+        times -= 1
     word = ''
-    times -= 1
 
 
 
