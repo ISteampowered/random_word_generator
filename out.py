454c76d23fdc6ab0add2528076d024b7bbb70ab9
Steam <steampunkv@gmail.com>
2017-12-28 18:28:43 +0100

    added a way for the option to be given via arguments

--- out.py
+++ out.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 import random
+import sys
 from saveAndLoad import SaveAndLoad
 
 previous_char = ' '
 word = ''
-file_in = input("path to data file: ")
+if len(sys.argv) == 1:
+    file_in = input("path to data file: ")
+    times = int(input("how many words?: "))
+    min_word_length = int(input("what is the minimum word length?"))
+
+elif len(sys.argv) == 4:
+    file_in = sys.argv[1]
+    times = int(sys.argv[2])
+    min_word_length = int(sys.argv[3])
+else:
+    print("Syntax: {} {} {} {}".format(sys.argv[0], 'file_path', 'amount_of_words', 'min_word_length'))
+
 my_obj = SaveAndLoad(file_in)
 
-times = int(input("how many words?: "))
-min_word_length = int(input("what is the minimum word legnth?"))
 while times > 0:
     while True:
         previous_char = random.choices(my_obj.give_keys(previous_char), weights=my_obj.give_values(previous_char))[0]
