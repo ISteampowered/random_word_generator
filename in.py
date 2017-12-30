17c0f37ff4ed60caeef13b3f6601f8ae8dfb89a6
Steam <steampunkv@gmail.com>
2017-12-30 18:43:37 +0100

    added option to analyze word length in in.py

--- in.py
+++ in.py
@@ -5,11 +5,14 @@ from prwg import Prwg
 parser = argparse.ArgumentParser(description="analyses file and creates a JSON file used by out.py")
 
 parser.add_argument('file_loc', type=str, help="path to text file you want to analyze")
+parser.add_argument('-s', type=str, action='store_true', default=False,
+                    help="if you want to analyze the length of the words")
+
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-o', type=str, default='', metavar='FILE',  help='reads data from FILE and overwrites it, if the '
-                                                                     'file does not exist it will be created')
-group.add_argument('-r', type=str, default=[], nargs=2, metavar='FILE', help='reads data from first arg and writes to '
-                                                                             'the second')
+group.add_argument('-o', type=str, default='', metavar='FILE',
+                   help='reads data from FILE and overwrites it, if the file does not exist it will be created')
+group.add_argument('-r', type=str, default=[], nargs=2, metavar='FILE',
+                   help='reads data from first arg and writes to the second')
 group.add_argument('-w', type=str, default='', metavar='FILE', help='writes to FILE')
 args = parser.parse_args()
 
@@ -25,4 +28,5 @@ else:
     datafile_name_in = ''
     datafile_name_out = args.w
 
-Prwg.analyse(fileLoc, datafile_name_out=datafile_name_out, datafile_name_in=datafile_name_in)
+Prwg.analyse(fileLoc, datafile_name_out=datafile_name_out, datafile_name_in=datafile_name_in,
+             analyze_word_length=args.s)
