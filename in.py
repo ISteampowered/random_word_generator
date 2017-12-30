2e1cf6d803764698d9eec1759cc892a5b96b54e3
Steam <steampunkv@gmail.com>
2017-12-30 11:07:12 +0100

    changed in.py so it made use of prwg.py

--- in.py
+++ in.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-import re
 import argparse
-from saveAndLoad import SaveAndLoad
+from prwg import Prwg
 
 parser = argparse.ArgumentParser(description="analyses file and creates a JSON file used by out.py")
 
@@ -26,21 +25,4 @@ else:
     datafile_name_in = ''
     datafile_name_out = args.w
 
-my_obj = SaveAndLoad(datafile_name_in)
-
-fileN = open(fileLoc, "r")
-
-
-for line in fileN:
-    line = line.split()
-    for word in line:
-        i = 0
-        word = re.sub('[^a-zA-Z]', '', word.lower())
-        word = word.center(len(word) + 2)
-
-        while i < len(word)-1:
-            my_obj.add(word[i:i + 2])
-            i += 1
-
-
-my_obj.save_to_file(datafile_name_out)
+Prwg.analyse(fileLoc, datafile_name_out=datafile_name_out, datafile_name_in=datafile_name_in)
