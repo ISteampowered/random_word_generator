3aff997ab0061bca11abb470c181d2e6e5bab1ca
Steam <steampunkv@gmail.com>
2018-01-01 21:46:54 +0100

    added look quantifier to in.py

--- in.py
+++ in.py
@@ -5,8 +5,11 @@ from prwg import Prwg
 parser = argparse.ArgumentParser(description="analyses file and creates a JSON file used by out.py")
 
 parser.add_argument('file_loc', type=str, help="path to text file you want to analyze")
-parser.add_argument('-s', action='store_true', default=False,
-                 help="if you want to analyze the length of the words")
+
+group2 = parser.add_mutually_exclusive_group()
+group2.add_argument('-s', action='store_true', default=False,
+                    help="if you want to analyze the length of the words")
+group2.add_argument('-l', type=int, default=1, help='sets the look back')
 
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-o', type=str, default='', metavar='FILE',
@@ -31,4 +34,5 @@ else:
 if args.s:
     Prwg.analyse_word_length(fileLoc, datafile_name_out=datafile_name_out, datafile_name_in=datafile_name_in)
 else:
-    Prwg.analyse(fileLoc, datafile_name_out=datafile_name_out, datafile_name_in=datafile_name_in)
+    Prwg.analyse(fileLoc, datafile_name_out=datafile_name_out, datafile_name_in=datafile_name_in,
+                 look_back_amount=args.l)
