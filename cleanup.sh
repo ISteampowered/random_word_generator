39e6ec98015204d2f9cde7d8c6e6556904fd8fbc
Steam <steampunkv@gmail.com>
2018-01-05 16:23:24 +0100

    cleanup.sh now also removes whitespace

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,4 @@ sed -i -n '/End of the Project Gutenberg EBook/q;p' $1
 sed -i -n '/^FOOTNOTES: /q;p' $1
 sed -i '/START OF THIS PROJECT GUTENBERG EBOOK /,$!d' $1
 sed -i 's/[^a-z ]/ /gI' $1 
-
+sed -i 's/ \+/ /g' $1
