40b6b5ad34356bef34cd28061e54f9c62c3c7797
Steam <steampunkv@gmail.com>
2018-01-05 17:57:07 +0100

    small bugfix

--- prwg.py
+++ prwg.py
@@ -27,7 +27,11 @@ class Prwg:
         if datafile_name_out == '' and not give_object:
             raise IOError('No data output given.')
 
-        my_obj = SaveAndLoad(file_loc=datafile_name_in)
+        try:
+            my_obj = SaveAndLoad(file_loc=datafile_name_in)
+        except FileNotFoundError:
+            my_obj = SaveAndLoad()
+
         file_in = open(file_loc, "r")
         previous_length = 0
 
